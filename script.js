c9c1d2c7a49dd9d6332ca065d320434b026e8fcf
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-09-27 12:34:37 -0500

    Fixed barebones display

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const nameDisplay = document.querySelector('#name-display')
 const nameDisplayCivilian = document.querySelector('#civilian-name-display')
 const imageEl = document.querySelector('#picDisplay')
 
+/* Character stats elements */
+
 const intelligenceStat = document.querySelector('#intelligence-stat')
 const strengthStat = document.querySelector('#strength-stat')
 const speedStat = document.querySelector('#speed-stat')
@@ -20,7 +22,7 @@ const combatStat = document.querySelector('#combat-stat')
 const birthplaceStat = document.querySelector('#birthplace-stat')
 
 const raceStat = document.querySelector('#race-stat')
-const heightStat = document.querySelectwor('#height-stat')
+const heightStat = document.querySelector('#height-stat')
 const weightStat = document.querySelector('#weight-stat')
 const genderStat = document.querySelector('#gender-stat')
 
@@ -30,6 +32,18 @@ const homeBaseStat = document.querySelector('#home-base-stat')
 const firstAppearanceStat = document.querySelector('#first-appearance-stat')
 const publisherStat = document.querySelector('#publisher-stat')
 const alignmentStat = document.querySelector('#alignment-stat')
+
+const statsList = [intelligenceStat, strengthStat, speedStat, durabilityStat, powerStat, combatStat,
+    birthplaceStat,
+    raceStat, heightStat, weightStat, genderStat,
+    occupationStat, homeBaseStat,
+    firstAppearanceStat, publisherStat, alignmentStat,
+
+]
+
+for (element of statsList) {
+    console.log(element.textContent)
+}
 
 
 
@@ -58,36 +72,36 @@ buttonOne.addEventListener('click', async () => {
 
     /* Power stats */
 
-    console.log(responseSearch.data.results[0].powerstats.intelligence)
-    console.log(responseSearch.data.results[0].powerstats.strength)
-    console.log(responseSearch.data.results[0].powerstats.speed)
-    console.log(responseSearch.data.results[0].powerstats.durability)
-    console.log(responseSearch.data.results[0].powerstats.power)
-    console.log(responseSearch.data.results[0].powerstats.combat)
+    intelligenceStat.textContent = responseSearch.data.results[0].powerstats.intelligence
+    strengthStat.textContent = responseSearch.data.results[0].powerstats.strength
+    speedStat.textContent = responseSearch.data.results[0].powerstats.speed
+    durabilityStat.textContent = responseSearch.data.results[0].powerstats.durability
+    powerStat.textContent = responseSearch.data.results[0].powerstats.power
+    combatStat.textContent = responseSearch.data.results[0].powerstats.combat
 
     /* Bio info */ console.log('================================')
 
-    console.log(responseSearch.data.results[0].biography['place-of-birth'])
-    console.log(responseSearch.data.results[0].biography['full-name'])
+    birthplaceStat.textContent = responseSearch.data.results[0].biography['place-of-birth']
+    nameDisplayCivilian.textContent = responseSearch.data.results[0].biography['full-name']
 
     /* Race, height, weight */ console.log('================================')
 
-    console.log(responseSearch.data.results[0].appearance.gender)
-    console.log(responseSearch.data.results[0].appearance.race)   
-    console.log(responseSearch.data.results[0].appearance.height[0])
-    console.log(responseSearch.data.results[0].appearance.weight[0])
+    genderStat.textContent = responseSearch.data.results[0].appearance.gender
+    raceStat.textContent = responseSearch.data.results[0].appearance.race   
+    heightStat.textContent = responseSearch.data.results[0].appearance.height[0]
+    weightStat.textContent = responseSearch.data.results[0].appearance.weight[0]
 
     /* Occupation */ console.log('================================')
 
-    console.log(responseSearch.data.results[0].work.occupation)
-    console.log(responseSearch.data.results[0].work.base)
+    occupationStat.textContent = responseSearch.data.results[0].work.occupation
+    homeBaseStat.textContent = responseSearch.data.results[0].work.base
 
 
     /* Publisher and First appearance */ console.log('================================')
 
-    console.log(responseSearch.data.results[0].biography.publisher)
-    console.log(responseSearch.data.results[0].biography['first-appearance'])
-    console.log(responseSearch.data.results[0].biography.alignment)
+    publisherStat.textContent = responseSearch.data.results[0].biography.publisher
+    firstAppearanceStat.textContent = responseSearch.data.results[0].biography['first-appearance']
+    alignmentStat.textContent = responseSearch.data.results[0].biography.alignment
 
 })
 
